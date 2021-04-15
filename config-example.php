eebf47de8b47394d396a769d742cc0f20007a1d6
codepoet80 <codepoet>
2021-04-15 22:49:30 +0000

    fix icon config

--- config-example.php
+++ config-example.php
@@ -4,7 +4,7 @@ return array(
         'OneOrMoreSecretsTheClientAndServerShare'
     ),	//This secret needs to be known to the client
     'title' => 'Your Chat!',
-    'alt-image' => '',	//leave empty to use the default
+    'alticon' => '',	//leave empty to use the default
     'welcomemessage' => 'Describe the purpose of your chat for visitors!'
     'allowedhtml' => '<p><b><i><u><br><ul><li><font>',
     'chatfile' => 'data/chatlog.json',
