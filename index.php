fffc2cbdfff98a65ec6e1d6663ad8784103c409b
CodePoet80 <jonathan.m.wise@gmail.com>
2021-04-16 12:08:35 -0400

    moves some styles to seperate file

--- index.php
+++ index.php
@@ -13,19 +13,9 @@
 <head>
 <title><?php echo $config['title']?></title>
 <style>
-    * { font-family:arial;}
     a { text-decoration: none; color:<?php echo $config['linkcolor']?>; }
     a:hover { text-decoration: underline; color:<?php echo $config['linkcolor']?>; }
-    .page-title { font-size: 28px; padding-bottom: 18px; display: flex; align-items: center;}
     .page-title::before { content: url('<?php echo $icon ?>.png'); margin-right: 18px; }
-    .description { float:right; text-align: right;  margin-right: 2%; max-width: 80%; font-size: 14px; font-style: italic; color: dimgray;}
-    .message-area { clear: both; padding-top: 20px; }
-    .message-group { border-bottom: 1px solid gray; margin: -4px; padding-left: 8px; clear: both }
-    .sender { font-weight: bold; }
-    .timestamp { margin-top: -12px; }
-    .footer { margin-top: 18px; font-size: 12px; color: dimgray;}
-    small { font-size: 11px; color: dimgray; }
-    strong { font-size: 26px; font-weight:bold; }
 </style>
 <link rel="shortcut icon" href="<?php echo $icon ?>.ico">
 <link rel="stylesheet" href="style.css">
